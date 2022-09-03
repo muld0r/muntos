5b129e745d57380ff4de0a0306eedee2f47cce38
Chris Copeland <chris@chrisnc.net>
2022-09-03 08:46:30 -0700

    use relaxed memory order for ticks

--- src/tick.c
+++ src/tick.c
@@ -8,11 +8,11 @@ static atomic_ulong rt_ticks;
 
 void rt_tick_advance(void)
 {
-    atomic_fetch_add_explicit(&rt_ticks, 1, memory_order_release);
+    atomic_fetch_add_explicit(&rt_ticks, 1, memory_order_relaxed);
     rt_yield();
 }
 
 unsigned long rt_tick(void)
 {
-    return atomic_load_explicit(&rt_ticks, memory_order_acquire);
+    return atomic_load_explicit(&rt_ticks, memory_order_relaxed);
 }
