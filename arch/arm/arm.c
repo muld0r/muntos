9ffae033dbe53566e56cbf385dcb573cc5678790
Chris Copeland <chris@chrisnc.net>
2022-09-22 02:38:53 -0700

    save/restore exception lr, switch stacks in rt_start

--- arch/arm/arm.c
+++ arch/arm/arm.c
@@ -6,21 +6,14 @@
 
 struct gp_context
 {
-    // Saved by context switch code.
-    uint32_t r11, r10, r9, r8, r7, r6, r5, r4;
+    // Saved by task context switch.
+    uint32_t r4, r5, r6, r7, r8, r9, r10, r11, exc_lr;
 
     // Saved automatically on exception entry.
     uint32_t r0, r1, r2, r3, r12;
     void (*lr)(void);
     void (*pc)(void);
     uint32_t apsr;
-};
-
-struct fp_context
-{
-    float s[16];
-    uint32_t fpscr;
-    uint32_t : 32;
 };
 
 void *rt_context_create(void *stack, size_t stack_size, void (*fn)(void))
@@ -44,19 +37,24 @@ void idle_fn(void)
 
 void rt_start(void)
 {
-    static char idle_stack[sizeof(struct gp_context)];
-    static struct rt_task idle;
-    rt_task_init(&idle, idle_fn, idle_stack, sizeof idle_stack, "idle", 0);
+    static char idle_stack[72];
 
-    // Set the process stack pointer as though the idle task is executing.
-    // The first context switch will push a context onto the idle stack.
-    // TODO: actually cause the idle task to execute.
-    char *idle_stack_end = idle_stack + sizeof idle_stack;
-    __asm__ __volatile__("msr psp, %0" : :"r"(idle_stack_end));
+    // TODO: Set the priorities of the tick and syscall interrupts and enable them.
 
-    // TODO: Set the priorities of the tick and syscall interrupts.
-    // enable them, and trigger the first one.
-    rt_sched();
+    // Set the process stack pointer to the top of the idle task stack.
+    __asm__ __volatile__(
+            "ldr r0, =0xE000ED08\n" // Load the address of VTOR.
+            "ldr r0, [r0]\n" // Read VTOR to get the vector address.
+            "ldr r0, [r0]\n" // Load the contents of the first vector entry.
+            "msr msp, r0\n" // Set the main stack pointer back to the start.
+            "msr psp, %0\n" // Set the process stack pointer to the idle task stack.
+            "mov r0, 3\n" // Switch to the process stack pointer and drop privileges.
+            "msr control, r0\n"
+            "isb\n"
+            : :"r"(&idle_stack[sizeof idle_stack]));
+
+    rt_yield();
+    idle_fn();
 }
 
 void rt_stop(void)
