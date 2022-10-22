fe2c27ff5b43072068840e4f4a5bee794f0ac3db
Chris Copeland <chris@chrisnc.net>
2022-10-22 14:25:16 -0700

    initialize h count and release mutex earlier on condition variable water solution

--- examples/water/cond.c
+++ examples/water/cond.c
@@ -12,6 +12,7 @@ struct reaction
 
 void reaction_init(struct reaction *rxn)
 {
+    rxn->h = 0;
     rt_cond_init(&rxn->hready);
     rt_cond_init(&rxn->hdone);
     rt_mutex_init(&rxn->m);
@@ -35,7 +36,7 @@ void oxygen(struct reaction *rxn)
     }
     make_water();
     rxn->h -= 2;
-    rt_cond_signal(&rxn->hdone);
-    rt_cond_signal(&rxn->hdone);
     rt_mutex_unlock(&rxn->m);
+    rt_cond_signal(&rxn->hdone);
+    rt_cond_signal(&rxn->hdone);
 }
