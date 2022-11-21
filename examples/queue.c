dd65a3c7c6b5ecdd14c43539c877967701af45d5
Chris Copeland <chris@chrisnc.net>
2022-11-20 23:46:16 -0800

    add timeout task to queue example

--- examples/queue.c
+++ examples/queue.c
@@ -1,6 +1,7 @@
 #include <rt/queue.h>
 #include <rt/rt.h>
 #include <rt/task.h>
+#include <rt/sleep.h>
 
 static const int n = 1000;
 
@@ -30,17 +31,33 @@ static void receiver(void *arg)
     rt_stop();
 }
 
+static volatile bool timed_out = false;
+
+static void timeout(void *arg)
+{
+    (void)arg;
+    rt_sleep(500);
+    timed_out = true;
+    rt_stop();
+}
+
 int main(void)
 {
-    RT_QUEUE_STATIC(queue, int, 40);
+    RT_QUEUE_STATIC(queue, int, 5);
     __attribute__((aligned(STACK_ALIGN))) static char
         sender_stack[TASK_STACK_SIZE],
-        receiver_stack[TASK_STACK_SIZE];
-    RT_TASK(sender, &queue, sender_stack, 1);
-    RT_TASK(receiver, &queue, receiver_stack, 1);
+        receiver_stack[TASK_STACK_SIZE],
+        timeout_stack[TASK_STACK_SIZE];
+    RT_TASK(sender, &queue, sender_stack, 2);
+    RT_TASK(receiver, &queue, receiver_stack, 2);
+    RT_TASK(timeout, NULL, timeout_stack, 1);
     rt_start();
     if (out_of_order)
     {
         return 1;
+    }
+    if (timed_out)
+    {
+        return 2;
     }
 }
