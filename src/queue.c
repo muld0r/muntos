b030ee4caff0fceb69538235ff7db9ba3393287b
Chris Copeland <chris@chrisnc.net>
2022-12-05 10:19:55 +0000

    refactor queue to look for a matching slot generation

--- src/queue.c
+++ src/queue.c
@@ -86,46 +86,40 @@ static size_t next(size_t q, size_t num_elems)
     return q;
 }
 
-static void advance(atomic_size_t *q, size_t num_elems)
-{
-    size_t e = rt_atomic_load_explicit(q, memory_order_relaxed);
-    while (!rt_atomic_compare_exchange_weak_explicit(q, &e, next(e, num_elems),
-                                                     memory_order_relaxed,
-                                                     memory_order_relaxed))
-    {
-    }
-}
-
 static void send(struct rt_queue *queue, const void *elem)
 {
     for (;;)
     {
         size_t enq = rt_atomic_load_explicit(&queue->enq, memory_order_relaxed);
-
-        /* Load the slot and attempt to claim it if it's empty. */
-        atomic_uchar *slot = &queue->slots[index(enq)];
-        unsigned char s = rt_atomic_load_explicit(slot, memory_order_relaxed);
-        if (sgen(s) != qsgen(enq))
+        atomic_uchar *slot;
+        unsigned char s;
+        for (;;)
         {
-            continue;
+            slot = &queue->slots[index(enq)];
+            s = rt_atomic_load_explicit(slot, memory_order_relaxed);
+            rt_logf("send: slot %zu %s\n", index(enq), state_str(state(s)));
+            if ((state(s) == SLOT_EMPTY) && (sgen(s) == qsgen(enq)))
+            {
+                break;
+            }
+            enq = next(enq, queue->num_elems);
         }
 
         const unsigned char send_s = sgen(s) | SLOT_SEND;
-        rt_logf("send: slot %zu %s\n", index(enq), state_str(state(s)));
-        if ((state(s) == SLOT_EMPTY) &&
-            rt_atomic_compare_exchange_strong_explicit(slot, &s, send_s,
+        if (rt_atomic_compare_exchange_strong_explicit(slot, &s, send_s,
                                                        memory_order_relaxed,
                                                        memory_order_relaxed))
         {
             rt_logf("send: slot %zu claimed...\n", index(enq));
-            advance(&queue->enq, queue->num_elems);
+            rt_atomic_store_explicit(&queue->enq, next(enq, queue->num_elems),
+                                     memory_order_relaxed);
 
-            s = send_s;
             unsigned char *const p = queue->data;
             memcpy(&p[queue->elem_size * index(enq)], elem, queue->elem_size);
 
             /* Use release order even on failure so that the memcpy is complete
              * before we mark the slot as empty again. */
+            s = send_s;
             if (rt_atomic_compare_exchange_strong_explicit(
                     slot, &s, sgen(s) | SLOT_FULL, memory_order_release,
                     memory_order_release))
@@ -136,8 +130,11 @@ static void send(struct rt_queue *queue, const void *elem)
             rt_logf("send: slot %zu skipped...\n", index(enq));
             /* If our slot has been skipped by a reader, then restore it
              * back to empty and keep looking. */
-            rt_atomic_store_explicit(slot, sgen(s) | SLOT_EMPTY,
-                                     memory_order_relaxed);
+            while (!rt_atomic_compare_exchange_weak_explicit(
+                slot, &s, sgen(s) | SLOT_EMPTY, memory_order_relaxed,
+                memory_order_relaxed))
+            {
+            }
         }
     }
     rt_sem_post(&queue->recv_sem);
@@ -148,47 +145,58 @@ static void recv(struct rt_queue *queue, void *elem)
     for (;;)
     {
         size_t deq = rt_atomic_load_explicit(&queue->deq, memory_order_relaxed);
-
-        atomic_uchar *slot = &queue->slots[index(deq)];
-        unsigned char s = rt_atomic_load_explicit(slot, memory_order_relaxed);
-
-        if (sgen(s) != qsgen(deq))
+        atomic_uchar *slot;
+        unsigned char s;
+        for (;;)
         {
-            continue;
-        }
-
-        rt_logf("recv: slot %zu %s\n", index(deq), state_str(state(s)));
-        if (state(s) == SLOT_SEND)
-        {
-            const unsigned char skipped_slot = s + SLOT_GEN_INCREMENT;
-            /* If we encounter an in-progress send, attempt to skip it by
-             * incrementing its generation. The send may have completed in the
-             * mean time, in which case we can attempt to read from the slot.
-             * If not, there will be full slots to receive after this slot,
-             * because the level allowing receivers to run is only incremented
-             * after each send is complete. */
-            if (rt_atomic_compare_exchange_strong_explicit(
-                    slot, &s, skipped_slot, memory_order_relaxed,
-                    memory_order_relaxed))
+            slot = &queue->slots[index(deq)];
+            s = rt_atomic_load_explicit(slot, memory_order_relaxed);
+            rt_logf("recv: slot %zu %s\n", index(deq), state_str(state(s)));
+            if (sgen(s) == qsgen(deq))
             {
-                /* If we successfully skipped the slot, advance the dequeue
-                 * index. */
-                advance(&queue->deq, queue->num_elems);
+                if (state(s) == SLOT_SEND)
+                {
+                    const unsigned char skipped_slot = s + SLOT_GEN_INCREMENT;
+                    /* If we encounter an in-progress send, attempt to skip it
+                     * by incrementing its generation. The send may have
+                     * completed in the mean time, in which case we can attempt
+                     * to read from the slot. If not, there will be full slots
+                     * to receive after this slot, because the level allowing
+                     * receivers to run is only incremented after each send is
+                     * complete. */
+                    rt_atomic_compare_exchange_strong_explicit(
+                        slot, &s, skipped_slot, memory_order_relaxed,
+                        memory_order_relaxed);
+                }
+                if ((state(s) == SLOT_FULL) || (state(s) == SLOT_RECV))
+                {
+                    break;
+                }
             }
+            deq = next(deq, queue->num_elems);
         }
-        else if ((state(s) == SLOT_FULL) &&
-                 rt_atomic_compare_exchange_strong_explicit(
-                     slot, &s, sgen(s) | SLOT_RECV, memory_order_acquire,
-                     memory_order_relaxed))
+
+        unsigned char recv_s = sgen(s) | SLOT_RECV;
+        if (rt_atomic_compare_exchange_strong_explicit(slot, &s, recv_s,
+                                                       memory_order_acquire,
+                                                       memory_order_relaxed))
         {
             rt_logf("recv: slot %zu claimed...\n", index(deq));
-            advance(&queue->deq, queue->num_elems);
+
             const unsigned char *const p = queue->data;
             memcpy(elem, &p[queue->elem_size * index(deq)], queue->elem_size);
+
             const unsigned char empty_s =
                 (sgen(s) + SLOT_GEN_INCREMENT) | SLOT_EMPTY;
-            rt_atomic_store_explicit(slot, empty_s, memory_order_relaxed);
-            break;
+            if (rt_atomic_compare_exchange_strong_explicit(
+                    slot, &recv_s, empty_s, memory_order_relaxed,
+                    memory_order_relaxed))
+            {
+                rt_atomic_store_explicit(&queue->deq,
+                                         next(deq, queue->num_elems),
+                                         memory_order_relaxed);
+                break;
+            }
         }
     }
     rt_sem_post(&queue->send_sem);
@@ -201,29 +209,44 @@ static void peek(struct rt_queue *queue, void *elem)
     for (;;)
     {
         size_t deq = rt_atomic_load_explicit(&queue->deq, memory_order_relaxed);
-
-        atomic_uchar *slot = &queue->slots[index(deq)];
-        unsigned char s = rt_atomic_load_explicit(slot, memory_order_relaxed);
-
-        if (sgen(s) != qsgen(deq))
+        atomic_uchar *slot;
+        unsigned char s;
+        for (;;)
         {
-            continue;
+            slot = &queue->slots[index(deq)];
+            s = rt_atomic_load_explicit(slot, memory_order_relaxed);
+            rt_logf("peek: slot %zu %s\n", index(deq), state_str(state(s)));
+            if (sgen(s) == qsgen(deq))
+            {
+                if ((state(s) == SLOT_FULL) || (state(s) == SLOT_RECV))
+                {
+                    break;
+                }
+            }
+            deq = next(deq, queue->num_elems);
         }
 
-        if ((state(s) == SLOT_FULL) &&
-            rt_atomic_compare_exchange_strong_explicit(slot, &s,
-                                                       sgen(s) | SLOT_RECV,
+        unsigned char recv_s = sgen(s) | SLOT_RECV;
+        if (rt_atomic_compare_exchange_strong_explicit(slot, &s, recv_s,
                                                        memory_order_acquire,
                                                        memory_order_relaxed))
         {
+            rt_logf("peek: slot %zu claimed...\n", index(deq));
+
             const unsigned char *const p = queue->data;
             memcpy(elem, &p[queue->elem_size * index(deq)], queue->elem_size);
-            rt_atomic_store_explicit(slot, sgen(s) | SLOT_FULL,
-                                     memory_order_relaxed);
-            break;
+
+            const unsigned char full_s =
+                (sgen(s) + SLOT_GEN_INCREMENT) | SLOT_FULL;
+            if (rt_atomic_compare_exchange_strong_explicit(
+                    slot, &recv_s, full_s, memory_order_relaxed,
+                    memory_order_relaxed))
+            {
+                break;
+            }
         }
     }
-    /* After we've peeked, another receiver may run. */
+    /* After peeking, another receiver may run. */
     rt_sem_post(&queue->recv_sem);
 }
 
