1f270128f8e4f91d16c3618030685dc3ddd62436
Chris Copeland <chris@chrisnc.net>
2023-01-15 03:18:53 -0800

    remove memory clobber from arm/r rt_syscall_pend

--- arch/arm/r/arm-r.c
+++ arch/arm/r/arm-r.c
@@ -145,7 +145,7 @@ void rt_logf(const char *fmt, ...)
 void rt_syscall_pend(void)
 {
     SYS_SSIR1 = SYS_SSIR1_KEY;
-    __asm__("dsb" ::: "memory");
+    __asm__("dsb");
     __asm__("isb");
 }
 #endif
