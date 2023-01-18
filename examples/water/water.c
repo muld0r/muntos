b3c890a1712d970eb6646284771c935235db3e1f
Chris Copeland <chris@chrisnc.net>
2023-01-17 22:58:01 -0800

    simplify water example

--- examples/water/water.c
+++ examples/water/water.c
@@ -21,13 +21,13 @@ void make_water(void)
 
 static void timeout(void)
 {
-    rt_sleep(TICKS_TO_RUN + 5);
+    rt_sleep(TICKS_TO_RUN);
     rt_stop();
 }
 
 static void oxygen_loop(void)
 {
-    while (rt_tick() < TICKS_TO_RUN)
+    for (;;)
     {
         oxygen();
         rt_atomic_fetch_add_explicit(&oxygen_bonded, 1, memory_order_relaxed);
@@ -36,7 +36,7 @@ static void oxygen_loop(void)
 
 static void hydrogen_loop(void)
 {
-    while (rt_tick() < TICKS_TO_RUN)
+    for (;;)
     {
         hydrogen();
         rt_atomic_fetch_add_explicit(&hydrogen_bonded, 1, memory_order_relaxed);
@@ -57,11 +57,19 @@ int main(void)
 
     rt_start();
 
-    unsigned w = rt_atomic_load(&water_formed);
-    unsigned h = rt_atomic_load(&hydrogen_bonded);
-    unsigned o = rt_atomic_load(&oxygen_bonded);
+    const unsigned w = rt_atomic_load(&water_formed);
+    const unsigned h = rt_atomic_load(&hydrogen_bonded);
+    const unsigned o = rt_atomic_load(&oxygen_bonded);
 
-    if ((w != o) || (w * 2 != h))
+    /* The oxygen or hydrogen may not have bonded by the time rt_stop is called
+     * after making a water molecule, so allow for o and h to be one molecule's
+     * worth below expected value or exactly equal to it. */
+    const unsigned o_lo = w - 1;
+    const unsigned o_hi = w;
+    const unsigned h_lo = (w - 1) * 2;
+    const unsigned h_hi = w * 2;
+
+    if ((o < o_lo) || (o > o_hi) || (h < h_lo) || (h > h_hi))
     {
         return 1;
     }
