10ea01217d4155e2b59000c3a68f979de4f1f7c9
Chris Copeland <chris@chrisnc.net>
2023-04-27 01:11:21 -0700

    use positive numbers for TASK_INITIAL_EXC_RETURN

--- arch/arm/m/exc_return.h
+++ arch/arm/m/exc_return.h
@@ -7,12 +7,12 @@
 #if __ARM_ARCH == 8
 
 // non-secure stack, thread mode, no FP, use PSP, non-secure exception
-#define TASK_INITIAL_EXC_RETURN -68
+#define TASK_INITIAL_EXC_RETURN 0xFFFFFFBC
 
 #elif (__ARM_ARCH == 7) || (__ARM_ARCH == 6)
 
 // thread mode, no FP, use PSP
-#define TASK_INITIAL_EXC_RETURN -3
+#define TASK_INITIAL_EXC_RETURN 0xFFFFFFFD
 
 #else
 #error "unsupported __ARM_ARCH"
